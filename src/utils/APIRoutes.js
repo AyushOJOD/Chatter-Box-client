a33edcee156bdad1227d1f1a58809dc823730442
Ayush <rishiustaad@gmail.com>
2024-01-19 18:45:11 +0530

    connecting backend to the frontend

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,5 @@
-export const host = "http://localhost:5000";
+// export const host = "http://localhost:5000";
+export const host = "https://chatterbox-server-uvzi.onrender.com";
 
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
